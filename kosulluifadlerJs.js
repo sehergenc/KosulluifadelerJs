ebe12fd82265a3289fcae50e3b14577f7da683c9
Seher Genç <gncshr01@hotmail.com>
2024-06-14 16:59:38 +0300

    Add files via upload

---
+++ kosulluifadlerJs.js
@@ -0,0 +1,14 @@
+let yas = 100;
+let ucret = 450;
+
+if (yas >= 65 && yas <= 100) {
+  ucret = 0;
+} else if (yas >= 25 && yas < 65) {
+  ucret = 450;
+} else if (yas >= 15 && yas < 25) {
+  ucret = 225;
+} else if (yas >= 10 && yas < 15) {
+  ucret = 90;
+} else if (yas >= 0 && yas < 10) {
+  ucret = 0;
+}
